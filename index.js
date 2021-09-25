c5c8c46dc36b8e63c34b196baaec60226221767e
nisha saraswat <nisha0217@gmail.com>
2021-09-25 19:48:42 +0200

    don't auto quit if its a macOS

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // BrowserWindow = a class that lets us create rendering/browser windows
 const { app, BrowserWindow } = require("electron");
 
+const isMac = process.platform === 'darwin';
+
+
 // Require path that let us build file path os independtly
 //const path = require("path");
 function createWindow() {
@@ -13,9 +16,18 @@ function createWindow() {
       title:"The Shoe Shop"
       //https://leverans.rubinbarclay.dev/
     });
-     mainWindow.loadURL("http://localhost:3000");  
+  mainWindow.loadURL("http://localhost:3000");
+  isMac && app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  
 }
     
     // load an URL in the window
 
 app.whenReady().then(createWindow);
+// Listen for if all windows have closed
+// if not a Mac the close the app
+app.on('window-all-closed', () => {
+  !isMac && app.quit;
+});
