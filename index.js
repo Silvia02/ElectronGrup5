8c4bd90372434e4b18d6ebb0d82a660cd13f37d2
Viviann Rydh <rydh.vivian@gmail.com>
2021-09-27 11:46:20 +0200

    add menu

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
-// Require som parts of electron
-// app = the application
-// BrowserWindow = a class that lets us create rendering/browser windows
-const { app, BrowserWindow } = require("electron");
-
-// Require path that let us build file path os independtly
-//const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
+const remoteMain = require('@electron/remote/main');
+remoteMain.initialize();
+let mainWindow;
 function createWindow() {
-    // create a new browser window
     const mainWindow = new BrowserWindow({
       width: 800,
       height: 600,
+      webPreferences: {
+        nativeWindowOpen: true,
+        nodeIntegration: true,
+        contextIsolation: false
+      }
       //https://leverans.rubinbarclay.dev/
     });
-     mainWindow.loadURL("http://localhost:3000");  
+  remoteMain.enable(mainWindow.webContents);
+  mainWindow.loadURL("https://leverans.rubinbarclay.dev/");  
+  mainWindow.show();
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
-    
-    // load an URL in the window
 
-app.whenReady().then(createWindow);
+function createMenu() {
+  let template = require('./menu-choices/menu.json');
+  let mac = require('./menu-choices/mac-specific.json');
+  if (process.platform === 'darwin') {
+    template.unshift(mac.appMenu);
+    let editMenu = template.find(x => x.label === 'Edit');
+    editMenu.submenu = [...editMenu.submenu, ...mac.speechChoices];
+    let windowMenu = template.find(x => x.role === 'window');
+    windowMenu.submenu = mac.windowChoices;
+  }
+  JSON.stringify(template, function (key, val) {
+    if (key === 'label' && !this.submenu && !this.role) {
+      this.click = (...args) => menuClickHandler(...args);
+    }
+    if (key === 'accelerator' && val instanceof Array) {
+      this.accelerator = process.platform === 'darwin' ? val[0] : val[1];
+    }
+    return val;
+  });
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+function menuClickHandler(menuItem) {
+  mainWindow.webContents.send('menuChoice', menuItem.label);
+}
+
+app.on('ready', createWindow);
+app.on('ready', createMenu);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
