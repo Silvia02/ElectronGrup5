69414bff5476e725630cef4cadf63e68f4449062
nisha saraswat <nisha0217@gmail.com>
2021-09-29 12:44:41 +0200

    icon added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon:__dirname+'/icon.ico',
     webPreferences: {
       nativeWindowOpen: true,
       nodeIntegration: true,
