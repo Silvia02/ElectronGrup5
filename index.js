2d1c45659ac4af5321314f941e75d20f39f8a854
Silvia02 <srj1213@gmail.com>
2021-09-29 21:57:25 +0200

    added width

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 700,
     webPreferences: {
       nativeWindowOpen: true,
       nodeIntegration: true,
